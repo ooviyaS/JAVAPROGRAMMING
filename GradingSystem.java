eaaac7b7c21864df3a66174eb3ed12d0299bc4bb
ooviyaS <ooviyadhanu@gmail.com>
2025-06-14 15:57:01 +0530

    Update GradingSystem.java

--- GradingSystem.java
+++ GradingSystem.java
@@ -1,24 +1,23 @@
-package Task;
-
 import java.util.Scanner;
 
 public class GradingSystem {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int mark = scanner.nextInt();
-        if (mark > 100) {
+        System.out.print("Enter mark: ");
+        int marks = scanner.nextInt();
+        if (marks > 100) {
             System.out.println("Invalid Input");
-        } else if (mark == 100) {
+        } else if (marks == 100) {
             System.out.println("S");
-        } else if (mark >= 90) {
+        } else if (marks >= 90) {
             System.out.println("A");
-        } else if (mark >= 80) {
+        } else if (marks >= 80) {
             System.out.println("B");
-        } else if (mark >= 70) {
+        } else if (marks >= 70) {
             System.out.println("C");
-        } else if (mark >= 60) {
+        } else if (marks >= 60) {
             System.out.println("D");
-        } else if (mark >= 50) {
+        } else if (marks >= 50) {
             System.out.println("E");
         } else {
             System.out.println("F");
@@ -26,4 +25,3 @@ public class GradingSystem {
         scanner.close();
     }
 }
-
