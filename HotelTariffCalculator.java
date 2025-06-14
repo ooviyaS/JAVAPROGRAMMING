cb2c2edf9d33cb82f65b761aa4e504ec349772d7
ooviyaS <ooviyadhanu@gmail.com>
2025-06-14 15:57:21 +0530

    Update HotelTariffCalculator.java

--- HotelTariffCalculator.java
+++ HotelTariffCalculator.java
@@ -1,29 +1,24 @@
-package Task;
-
 import java.util.Scanner;
 
 public class HotelTariffCalculator {
-
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter month (1-12): ");
+        System.out.print("Enter number: ");
         int month = scanner.nextInt();
-        System.out.print("Enter room rent per day: ");
         double rentPerDay = scanner.nextDouble();
-        System.out.print("Enter number of days stayed: ");
         int numberOfDays = scanner.nextInt();
+        double tariff;
 
-        double totalTariff;
-        double peakSeasonMultiplier = 1.2; 
         switch (month) {
             case 4: case 5: case 6: case 11: case 12:
-                totalTariff = rentPerDay * numberOfDays * peakSeasonMultiplier;
+                tariff = rentPerDay * numberOfDays * 1.2;
                 break;
             default:
-                totalTariff = rentPerDay * numberOfDays;
+                tariff = rentPerDay * numberOfDays;
+                break;
         }
-        System.out.printf("Total Tariff: %.2f%n", totalTariff);
+
+        System.out.printf("%.2f%n", tariff);
         scanner.close();
     }
 }
-
